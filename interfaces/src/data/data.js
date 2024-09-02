0f3f820ca73a4fec566ba6eeecc8929fb76303d7
henriquemota <mota.henrique@gmail.com>
2024-09-02 09:13:54 -0300

    alteracao do codigo para pastas

---
+++ interfaces/src/data/data.js
@@ -0,0 +1,124 @@
+const data = [
+	{
+		nome: 'Teclado sem fio Logitech MX Keys Mini com Iluminação Inteligente, Conexão USB ou Bluetooth Easy-Switch para até 3 dispositivos e Bateria Recarregável - Grafite',
+		valor: 660.0,
+		imagem: require('../../assets/teclado.jpg'),
+	},
+	{
+		nome: 'Mouse sem fio Logitech MX Master 3S com Sensor Darkfield para Uso em Qualquer Superfície, Design Ergonômico, Clique Silencioso, Conexão USB ou Bluetooth - Grafite',
+		valor: 587.02,
+		imagem: require('../../assets/mouse.jpg'),
+	},
+	{
+		nome: 'Apple notebook MacBook Air de 13 polegadas: Chip M2 da Apple com CPU de oito núcleos e GPU de oito núcleos, de 256 GB SSD - Cinza espacial',
+		valor: 7699.99,
+		imagem: require('../../assets/macbook.jpg'),
+	},
+	{
+		nome: 'Multifuncional Epson EcoTank L5590 - Tanque de Tinta Colorida, Wi-Fi Direct, Ethernet, Fax, ADF, Bivolt, Preta',
+		valor: 1619.1,
+		imagem: require('../../assets/impressora.jpg'),
+	},
+	{
+		nome: 'Teclado sem fio Logitech MX Keys Mini com Iluminação Inteligente, Conexão USB ou Bluetooth Easy-Switch para até 3 dispositivos e Bateria Recarregável - Grafite',
+		valor: 660.0,
+		imagem: require('../../assets/teclado.jpg'),
+	},
+	{
+		nome: 'Mouse sem fio Logitech MX Master 3S com Sensor Darkfield para Uso em Qualquer Superfície, Design Ergonômico, Clique Silencioso, Conexão USB ou Bluetooth - Grafite',
+		valor: 587.02,
+		imagem: require('../../assets/mouse.jpg'),
+	},
+	{
+		nome: 'Apple notebook MacBook Air de 13 polegadas: Chip M2 da Apple com CPU de oito núcleos e GPU de oito núcleos, de 256 GB SSD - Cinza espacial',
+		valor: 7699.99,
+		imagem: require('../../assets/macbook.jpg'),
+	},
+	{
+		nome: 'Multifuncional Epson EcoTank L5590 - Tanque de Tinta Colorida, Wi-Fi Direct, Ethernet, Fax, ADF, Bivolt, Preta',
+		valor: 1619.1,
+		imagem: require('../../assets/impressora.jpg'),
+	},
+	{
+		nome: 'Teclado sem fio Logitech MX Keys Mini com Iluminação Inteligente, Conexão USB ou Bluetooth Easy-Switch para até 3 dispositivos e Bateria Recarregável - Grafite',
+		valor: 660.0,
+		imagem: require('../../assets/teclado.jpg'),
+	},
+	{
+		nome: 'Mouse sem fio Logitech MX Master 3S com Sensor Darkfield para Uso em Qualquer Superfície, Design Ergonômico, Clique Silencioso, Conexão USB ou Bluetooth - Grafite',
+		valor: 587.02,
+		imagem: require('../../assets/mouse.jpg'),
+	},
+	{
+		nome: 'Apple notebook MacBook Air de 13 polegadas: Chip M2 da Apple com CPU de oito núcleos e GPU de oito núcleos, de 256 GB SSD - Cinza espacial',
+		valor: 7699.99,
+		imagem: require('../../assets/macbook.jpg'),
+	},
+	{
+		nome: 'Multifuncional Epson EcoTank L5590 - Tanque de Tinta Colorida, Wi-Fi Direct, Ethernet, Fax, ADF, Bivolt, Preta',
+		valor: 1619.1,
+		imagem: require('../../assets/impressora.jpg'),
+	},
+	{
+		nome: 'Teclado sem fio Logitech MX Keys Mini com Iluminação Inteligente, Conexão USB ou Bluetooth Easy-Switch para até 3 dispositivos e Bateria Recarregável - Grafite',
+		valor: 660.0,
+		imagem: require('../../assets/teclado.jpg'),
+	},
+	{
+		nome: 'Mouse sem fio Logitech MX Master 3S com Sensor Darkfield para Uso em Qualquer Superfície, Design Ergonômico, Clique Silencioso, Conexão USB ou Bluetooth - Grafite',
+		valor: 587.02,
+		imagem: require('../../assets/mouse.jpg'),
+	},
+	{
+		nome: 'Apple notebook MacBook Air de 13 polegadas: Chip M2 da Apple com CPU de oito núcleos e GPU de oito núcleos, de 256 GB SSD - Cinza espacial',
+		valor: 7699.99,
+		imagem: require('../../assets/macbook.jpg'),
+	},
+	{
+		nome: 'Multifuncional Epson EcoTank L5590 - Tanque de Tinta Colorida, Wi-Fi Direct, Ethernet, Fax, ADF, Bivolt, Preta',
+		valor: 1619.1,
+		imagem: require('../../assets/impressora.jpg'),
+	},
+	{
+		nome: 'Teclado sem fio Logitech MX Keys Mini com Iluminação Inteligente, Conexão USB ou Bluetooth Easy-Switch para até 3 dispositivos e Bateria Recarregável - Grafite',
+		valor: 660.0,
+		imagem: require('../../assets/teclado.jpg'),
+	},
+	{
+		nome: 'Mouse sem fio Logitech MX Master 3S com Sensor Darkfield para Uso em Qualquer Superfície, Design Ergonômico, Clique Silencioso, Conexão USB ou Bluetooth - Grafite',
+		valor: 587.02,
+		imagem: require('../../assets/mouse.jpg'),
+	},
+	{
+		nome: 'Apple notebook MacBook Air de 13 polegadas: Chip M2 da Apple com CPU de oito núcleos e GPU de oito núcleos, de 256 GB SSD - Cinza espacial',
+		valor: 7699.99,
+		imagem: require('../../assets/macbook.jpg'),
+	},
+	{
+		nome: 'Multifuncional Epson EcoTank L5590 - Tanque de Tinta Colorida, Wi-Fi Direct, Ethernet, Fax, ADF, Bivolt, Preta',
+		valor: 1619.1,
+		imagem: require('../../assets/impressora.jpg'),
+	},
+	{
+		nome: 'Teclado sem fio Logitech MX Keys Mini com Iluminação Inteligente, Conexão USB ou Bluetooth Easy-Switch para até 3 dispositivos e Bateria Recarregável - Grafite',
+		valor: 660.0,
+		imagem: require('../../assets/teclado.jpg'),
+	},
+	{
+		nome: 'Mouse sem fio Logitech MX Master 3S com Sensor Darkfield para Uso em Qualquer Superfície, Design Ergonômico, Clique Silencioso, Conexão USB ou Bluetooth - Grafite',
+		valor: 587.02,
+		imagem: require('../../assets/mouse.jpg'),
+	},
+	{
+		nome: 'Apple notebook MacBook Air de 13 polegadas: Chip M2 da Apple com CPU de oito núcleos e GPU de oito núcleos, de 256 GB SSD - Cinza espacial',
+		valor: 7699.99,
+		imagem: require('../../assets/macbook.jpg'),
+	},
+	{
+		nome: 'Multifuncional Epson EcoTank L5590 - Tanque de Tinta Colorida, Wi-Fi Direct, Ethernet, Fax, ADF, Bivolt, Preta',
+		valor: 1619.1,
+		imagem: require('../../assets/impressora.jpg'),
+	},
+]
+
+export default data
