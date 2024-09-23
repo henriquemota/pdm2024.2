dcf8ce439734aa2abaca2915b039ae5255d05d85
henriquemota <mota.henrique@gmail.com>
2024-09-23 08:13:07 -0300

    inicio do codigo

--- navegacao/app/index.jsx
+++ navegacao/app/index.jsx
@@ -1,8 +1,19 @@
+import { Link } from 'expo-router'
 import React from 'react'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Index = () => {
-	return <View></View>
+	return (
+		<SafeAreaView style={{ flex: 1, paddingHorizontal: 4 }}>
+			<View>
+				<Text>Bem vindo ao React Native</Text>
+				<Link href={{ pathname: '/lista' }} style={{ padding: 16, borderWidth: 1, borderRadius: 4 }}>
+					Vai para lista
+				</Link>
+			</View>
+		</SafeAreaView>
+	)
 }
 
 export default Index
