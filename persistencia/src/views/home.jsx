9ca9862d2fd5d4c70ae3fded5746c6148eda7958
henriquemota <mota.henrique@gmail.com>
2024-10-29 19:24:01 -0300

    implementacao do asyncstorage

--- persistencia/src/views/home.jsx
+++ persistencia/src/views/home.jsx
@@ -1,58 +1,27 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Alert, View } from 'react-native'
-import { Button, TextInput } from 'react-native-paper'
+import { Button, Text, TextInput } from 'react-native-paper'
 
 const Home = () => {
-	const [nome, setNome] = useState('')
+	const [user, setUser] = useState(null)
 
-	const saveData = async () => {
-		try {
-			await AsyncStorage.setItem('dados_usuario', nome)
-			Alert.alert('Sucesso', 'Dados gravados com sucesso.')
-		} catch (e) {
-			Alert.alert('Erro', 'Erro ao gravar os dados.')
-		}
-	}
-
-	const clearData = async (value) => {
-		try {
-			await AsyncStorage.removeItem(value)
-		} catch (e) {
-			Alert.alert('Erro', 'Erro ao remover os dados.')
-		}
-	}
-
-	const clearAllData = async () => {
-		try {
-			await AsyncStorage.clear()
-		} catch (e) {
-			Alert.alert('Erro', 'Erro ao remover os dados.')
-		}
-	}
-
-	const getData = async () => {
-		try {
-			const data = await AsyncStorage.getItem('dados_usuario')
-			setNome(data)
-		} catch (e) {
-			console.log(e)
-			Alert.alert('Erro', 'Erro ao recuperar os dados.')
-		}
-	}
+	useEffect(() => {
+		AsyncStorage.getItem('user').then((data) => setUser(JSON.parse(data)))
+	}, [])
 
 	return (
-		<View>
-			<TextInput onChangeText={setNome} value={nome} />
-			<View style={{ flexDirection: 'row', justifyContent: 'center', gap: 4, margin: 8 }}>
-				<Button mode='outlined' onPress={saveData}>
-					Gravar
-				</Button>
-				<Button mode='outlined' onPress={getData}>
-					Recuperar dados
-				</Button>
-			</View>
-			{/* <Text>{userSerialized}</Text> */}
+		<View style={{ padding: 4 }}>
+			<TextInput value={user?.login} />
+			<Button
+				onPress={async () => {
+					await AsyncStorage.setItem('user', JSON.stringify(user))
+					Alert.alert('Mensagem', 'Dados gravados com sucesso')
+				}}
+			>
+				Valor
+			</Button>
+			<Text>{user && JSON.stringify(user)}</Text>
 		</View>
 	)
 }
